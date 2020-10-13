b5bd5ab6ea3d80f5ab725bbae5ef5d66821de3c2
S1B41 <sibai.haj.mousa@tuhh.de>
2020-10-13 13:01:13 +0200

    remove max typo3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' =>
     [
         'depends' => [
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '6.2.0',
         ],
         'conflicts' => [
         ],
