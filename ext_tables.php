ab5017b2408bd85c4446e0893b067d16d5d46a72
kevinrodriguesnobre <krn.github@gmail.com>
2022-01-27 14:12:23 +0100

    feature/CP-2848-typo3-fix (#2)

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,4 @@
 defined('TYPO3_MODE') or die();
 
 // Add static typoscript configurations
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'CleverPush');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('cleverpush', 'Configuration/TypoScript/', 'CleverPush');
